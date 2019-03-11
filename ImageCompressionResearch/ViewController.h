5d0467d56623e1f56907a0ca67bb60960345bd9c
qinyuelong <qinyuelong.cn@gmail.com>
2019-03-11 09:14:33 +0800

    first commit

---
+++ ImageCompressionResearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageCompressionResearch
+//
+//  Created by apple on 2019/3/11.
+//  Copyright © 2019 yinhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImageCompressionResearch/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ImageCompressionResearch
+//
+//  Created by apple on 2019/3/11.
+//  Copyright © 2019 yinhe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
